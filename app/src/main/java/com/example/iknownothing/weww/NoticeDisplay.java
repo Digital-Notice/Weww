17195cd7ff417e09eaeeb6bef1bad6359792df5d
saharsh <saharshpandeybolt@gmail.com>
2018-10-05 10:25:23 +0530

    CardView Display of Notice

---
+++ app/src/main/java/com/example/iknownothing/weww/NoticeDisplay.java
@@ -0,0 +1,13 @@
+package com.example.iknownothing.weww;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_display);
+    }
+}
