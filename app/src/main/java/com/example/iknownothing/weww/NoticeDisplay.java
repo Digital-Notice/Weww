a43b39d4717b4c8d0f88fc72c0814697ba5f73c0
saharsh <saharshpandeybolt@gmail.com>
2018-10-08 23:56:44 +0530

    Validated onPageScrollStateChanged

--- app/src/main/java/com/example/iknownothing/weww/NoticeDisplay.java
+++ app/src/main/java/com/example/iknownothing/weww/NoticeDisplay.java
@@ -3,6 +3,7 @@ package com.example.iknownothing.weww;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,21 +28,21 @@ public class NoticeDisplay extends AppCompatActivity {
                 "Thank you for the opportunities for professional and personal development that you have provided me during the last five years.\n";
         models.add(new Model(R.drawable.ic_launcher_background,"Diwali Vacation","8/Oct","7:20 PM","Auditorium",descrp,"Bhupal Arya"));
         models.add(new Model(R.drawable.ic_launcher_background,"Zion Fest","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
-        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Abhyud Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Audaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Abhudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"daya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Abhyu Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Auddaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"ddaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+        models.add(new Model(R.drawable.ic_launcher_background,"hyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
 
         adapter = new Adapter(models,this);
 
         viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
-        viewPager.setClipToPadding(false);
-        viewPager.setClipChildren(false);
+        //viewPager.setClipToPadding(false);
+        //viewPager.setClipChildren(false);
         viewPager.setPadding(40,0,40,0);
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -57,7 +58,8 @@ public class NoticeDisplay extends AppCompatActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
-                if(state < models.size())
+                Log.d("hello",String.valueOf(state));
+                if(state == models.size()-1)
                 {
                     viewPager.setCurrentItem(0);
                 }
