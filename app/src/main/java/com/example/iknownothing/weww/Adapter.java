20fa4bafa6fce1d8293d6033db988ae74554f326
saharsh <saharshpandeybolt@gmail.com>
2018-10-08 19:29:51 +0530

    Validated ViewPagerAdapter

--- app/src/main/java/com/example/iknownothing/weww/Adapter.java
+++ app/src/main/java/com/example/iknownothing/weww/Adapter.java
@@ -1,4 +1,52 @@
 package com.example.iknownothing.weww;
 
-public class Adapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Adapter extends PagerAdapter{
+
+    private List<Model> models;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public Adapter(List<Model> models,Context context)
+    {
+        this.models = models;
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.all_notice_layout,container,false);
+
+        TextView title,date,time,venue,details,updatedBy;
+
+        return super.instantiateItem(container, position);
+
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
 }
--- app/src/main/java/com/example/iknownothing/weww/NoticeDisplay.java
+++ app/src/main/java/com/example/iknownothing/weww/NoticeDisplay.java
@@ -1,13 +1,39 @@
 package com.example.iknownothing.weww;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NoticeDisplay extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_display);
+
+
+        models = new ArrayList<>();
+        String descrp="Please accept this letter as formal notification that I am resigning from my position with ABCD Company on Friday, March XX, 20XX. I understand that two weeks notice is standard; however, personal circumstances require that I leave my position at this company by the end of this week.\n" +
+                "\n" +
+                "I am glad to provide any assistance I can during this transition.\n" +
+                "\n" +
+                "Thank you for the opportunities for professional and personal development that you have provided me during the last five years.\n";
+        models.add(new Model(R.drawable.ic_launcher_background,"Diwali Vacation","8/Oct","7:20 PM","Auditorium",descrp,"Bhupal Arya"));
+        models.add(new Model(R.drawable.ic_launcher_background,"Abhyudaya Interview","13/Oct","12:00","COER CAFETARIA",descrp,"Saharsh Pandey"));
+
+        adapter = new Adapter(models,this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130,0,130,0);
+
+
     }
 }
